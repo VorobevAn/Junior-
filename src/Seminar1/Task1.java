9f05d7748a7d8c570d25b57a4e3147057128462c
Andrey <cust89@mail.ru>
2023-12-05 21:01:29 +0500

    seminar1

--- src/Seminar1/Task1.java
+++ src/Seminar1/Task1.java
@@ -1,2 +1,12 @@
-package Seminar1;public class Task1 {
+package Seminar1;
+
+
+import java.util.Arrays;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int[] intList = {2, 4, -6, 2, 6, 7, 10, 9};
+        System.out.println(Arrays.stream(intList).filter(i -> i % 2 == 0).average().orElseThrow());
+    }
+
 }
